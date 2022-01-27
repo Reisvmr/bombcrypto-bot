5655068099ed54b0ad62b78f8ab785f2606f4496
reisvmr <reisvmr@gmail.com>
2022-01-27 14:33:20 -0300

    UP

--- BOMB-TELEGRAM/TELEGRAM-BOT/EstudoPoo.py
+++ BOMB-TELEGRAM/TELEGRAM-BOT/EstudoPoo.py
@@ -4,5 +4,17 @@ Created on Wed Jan 26 16:35:55 2022
 
 @author: reisvmr
 """
-
+##############################
 # POO
+##############################
+
+class Veiculo:
+   def __init__(self, nome, tipo):
+        self._nome = nome
+        self._tipo = tipo
+   def TipoVeiculo(Self):
+       print(Self._none)
+       
+v2 = Veiculo("Ferrari","Esportivo")       
+
+v2.tipoVeiculo()
