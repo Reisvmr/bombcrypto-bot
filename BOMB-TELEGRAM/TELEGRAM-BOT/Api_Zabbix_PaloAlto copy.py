87d8d7fab13fd0742ff490affe3e28ac0ab7e4b3
reisvmr <reisvmr@gmail.com>
2022-01-27 17:01:43 -0300

    UP

--- BOMB-TELEGRAM/TELEGRAM-BOT/Api_Zabbix_PaloAlto copy.py
+++ BOMB-TELEGRAM/TELEGRAM-BOT/Api_Zabbix_PaloAlto copy.py
@@ -18,11 +18,11 @@ urllib3.disable_warnings()
 #################################################
 ###########          Metodo Post       ########## 
 ############ Declarando as Variaveis ############
-
+IP = '10.32.208.101'
 chave = 'LUFRPT1idTV0YWlYbHd5ekozRjNOeE1kVnBHbC9lNUE9eTlGWXNncjJSekRpWE1MZ3hHcDVZYkhka3R5ZmoxSjZDV2RHTWxtOWtTUT0='
 categoriaurl = 'https://10.32.208.101/api/?type=report&async=yes&reporttype=predefined&reportname=top-url-categories&'
 type = 'op'
-query = '<show><vpn><flow><tunnel-id>6</tunnel-id></flow></vpn></show>'
+query = '<show><global-protect-portal><current-user><portal>GP-DECEA</portal></current-user></global-protect-portal></show>'
 #Payload contem todos os parametros que seram utilizados no post em forma de dicionario
 
 #'key' : chave}
@@ -38,7 +38,7 @@ query = '<show><vpn><flow><tunnel-id>6</tunnel-id></flow></vpn></show>'
 #
 
 def get_resquest(type=type,cmd=query,key=chave)
-    url_base = f'https://10.32.208.101/api/{chave}/{type}/{cmd}/'
+    url_base = f'https://10.32.208.101/api/?type={type}&cmd={cmd}&key={key}'
     r = requests.get(url_base)
     return r.json()
 
